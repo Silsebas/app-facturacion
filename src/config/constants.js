d7ee98803dd96c270ba73194eacc9955ccf97c46
Silsebas <jordancubillo@gmail.com>
2026-08-28 15:29:08 -0600

    feat: Actualizacion de precio

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,7 +6,7 @@ export const PRODUCTOS_PRECARGADOS = [
   'Café', 'Chirribisco', 'Empanadas', 'Gelatinas'
 ];
 
-export const PRECIOS_PRECARGADOS = [200, 300, 500, 600, 1000, 1200, 1500, 2000];
+export const PRECIOS_PRECARGADOS = [200, 300, 500, 600, 1000, 1200, 1300, 1500, 2000];
 export const BILLETES_RAPIDOS = [1000, 2000, 3000, 4000, 5000, 10000];
 export const METODOS_PAGO = ['Efectivo', 'SINPE', 'Pendiente de pago'];
 
