fdc576285c82b03e9aa8e2a8af768d1c047b7b50
Silsebas <jordancubillo@gmail.com>
2026-08-30 13:57:51 -0600

    Ajuste de precio

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,7 +5,7 @@ export const PRODUCTOS_PRECARGADOS = [
   'Tamales', 'Tacos', 'Pan dulce', 'Café', 'Copos', 'Empanadas', 'Gelatinas'
 ];
 
-export const PRECIOS_PRECARGADOS = [200, 300, 500, 700, 1000, 1500, 2000];
+export const PRECIOS_PRECARGADOS = [200, 300, 500, 700, 1000, 1300, 1500, 2000];
 export const BILLETES_RAPIDOS = [1000, 2000, 3000, 4000, 5000, 10000];
 export const METODOS_PAGO = ['Efectivo', 'SINPE', 'Pendiente de pago'];
 
