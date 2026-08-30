18f71646995c0fe0f1050a58686c3fbf43fa220c
Silsebas <jordancubillo@gmail.com>
2026-08-30 13:44:51 -0600

    Ajuste de precio

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,11 +2,10 @@
 export const PRODUCTOS_PRECARGADOS = [
   'Hamburguesa', 'Perro caliente', 'Picadillo', 'Arroz c/ pollo', 
   'Gallo de salchichón', 'Sándwich', 'Arroz c/ leche', 'Fresco', 
-  'Tamales', 'Tacos', 'Pan dulce', 'Tamal asado', 'Tamal de maicena', 
-  'Café', 'Chirribisco', 'Empanadas', 'Gelatinas'
+  'Tamales', 'Tacos', 'Pan dulce', 'Café', 'Copos', 'Empanadas', 'Gelatinas'
 ];
 
-export const PRECIOS_PRECARGADOS = [200, 300, 500, 600, 1000, 1200, 1300, 1500, 2000];
+export const PRECIOS_PRECARGADOS = [200, 300, 500, 700, 1000, 1500, 2000];
 export const BILLETES_RAPIDOS = [1000, 2000, 3000, 4000, 5000, 10000];
 export const METODOS_PAGO = ['Efectivo', 'SINPE', 'Pendiente de pago'];
 
